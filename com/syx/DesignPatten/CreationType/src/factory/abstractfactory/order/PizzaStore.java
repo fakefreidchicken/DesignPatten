88d2c5ec872ecb4677d6e6466511d3b98aa96c4c
fakefreidchicken <529576863@qq.com>
2023-08-10 20:59:01 +0800

    second

---
+++ com/syx/DesignPatten/CreationType/src/factory/abstractfactory/order/PizzaStore.java
@@ -0,0 +1,11 @@
+package factory.abstractfactory.order;
+
+public class PizzaStore {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//new OrderPizza(new BJFactory());
+		new OrderPizza(new LDFactory());
+	}
+
+}
